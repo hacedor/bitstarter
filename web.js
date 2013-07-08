fa32bbbbafa924422733fe415fc5bb77977bf7e2
Sourabh Meherchandani <sourabh.md@gmail.com>
2013-07-08 16:15:40 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,33 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+var fileName = "index.html";
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+//          console.log(data);
+          response.send(data);
+          fs.close(fd);
+        });
+      });
+    });
+  }
 });
+
+//  response.send('Hello World2!');
+});
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World2!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
